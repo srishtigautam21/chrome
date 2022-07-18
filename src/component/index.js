8adc534034ac5e64cd23488a217a2f0693e0e944
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2022-07-18 11:57:33 +0530

    Add:quotes

--- src/component/index.js
+++ src/component/index.js
@@ -2,3 +2,4 @@ export { Todo } from "./Todo/Todo";
 export { Greetings } from "./Greetings";
 export { Time } from "./Time";
 export { Weather } from "./Weather/Weather";
+export { Quotes } from "./Quotes/Quotes";
